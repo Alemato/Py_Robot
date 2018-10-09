bc231ab4802a7d12fc5b62aad0625e583d2bf337
stefano <stefano.decina@student.univaq.it>
2018-10-09 16:44:16 +0200

    per test del nodo switch motor

---
+++ py_robot/Node/prova_motor_switch.py
@@ -0,0 +1,30 @@
+#! /usr/bin/python
+
+import random
+import rospy
+import py_robot.msg as PyRobot
+
+switch = PyRobot.Motor_Switch_Node()
+
+
+def callback(msg):
+    velo = msg.velo
+    rospy.loginfo(velo)
+
+
+def main():
+    rospy.init_node('Motor_Switch_Node')
+    rospy.Subscriber("controller_pub", PyRobot.Controller_Node, callback)
+    switch_pub = rospy.Publisher("switch_sub", PyRobot.Motor_Switch_Node, queue_size=1)
+    r = rospy.Rate(1)
+    while not rospy.is_shutdown():
+        switch.switch = [random.choice(True, False), random.choice(True, False), random.choice(True, False)]
+        switch_pub.publish(switch)
+        r.sleep()
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        pass
