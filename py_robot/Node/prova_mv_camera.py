c7afc00e87e718f8d2e2b357d8f93ff7afff6951
stefano <stefano.decina@student.univaq.it>
2018-10-09 16:44:33 +0200

    per test del nodo mv camera

---
+++ py_robot/Node/prova_mv_camera.py
@@ -0,0 +1,24 @@
+#! /usr/bin/python
+
+import random
+import rospy
+import py_robot.msg as PyRobot
+
+mv_camera = PyRobot.MV_Camera_Node()
+
+
+def main():
+    rospy.init_node('MV_Camera_Node')
+    mv_camera_pub = rospy.Publisher("mv_camera_sub", PyRobot.MV_Camera_Node, queue_size=1)
+    r = rospy.Rate(5)
+    while not rospy.is_shutdown():
+        mv_camera.eureca = random.choice('dritto', 'destra', 'sinistra')
+        mv_camera_pub.publish(mv_camera)
+        r.sleep()
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        pass
