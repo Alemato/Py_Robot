98d30f1661f1643e92e9acdd4bb26c7ec7bc9ce8
stefano <stefano.decina@student.univaq.it>
2018-10-09 16:45:11 +0200

    per test del nodo sonar volt

---
+++ py_robot/Node/prova_sonar_volt.py
@@ -0,0 +1,26 @@
+#! /usr/bin/python
+
+import random
+import rospy
+import py_robot.msg as PyRobot
+
+
+sonar_volt = PyRobot.Sonar_Volt_Node
+
+
+def main():
+    rospy.init_node('Sonar_Volt_Node')
+    sonar_volt_pub = rospy.Publisher("sonar_volt_sub", PyRobot.Sonar_Volt_Node, queue_size=1)
+    r = rospy.Rate(1)
+    while not rospy.is_shutdown():
+        sonar_volt.sonar = [random.choice(1, 0), random.choice(1, 0), random.choice(1, 0)]
+        sonar_volt.volt = 12
+        sonar_volt_pub.publish(sonar_volt)
+        r.sleep()
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        pass
