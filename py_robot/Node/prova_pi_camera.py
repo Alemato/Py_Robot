8d5911bfe65a6a343971d185b96de39618cc0aae
stefano <stefano.decina@student.univaq.it>
2018-10-09 16:44:45 +0200

    per test del nodo pi camera

---
+++ py_robot/Node/prova_pi_camera.py
@@ -0,0 +1,25 @@
+#! /usr/bin/python
+
+import random
+import rospy
+import py_robot.msg as PyRobot
+
+
+pi_camera = PyRobot.Pi_Camera_Node()
+
+
+def main():
+    rospy.init_node('Pi_Camera_Node')
+    pi_camera_pub = rospy.Publisher("pi_camera_sub", PyRobot.Pi_Camera_Node, queue_size=1)
+    r = rospy.Rate(5)
+    while not rospy.is_shutdown():
+        pi_camera.visione = random.choice('dritto', 'destra', 'sinistra')
+        pi_camera_pub.publish(pi_camera)
+        r.sleep()
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        pass
